3c98a0cbd629cf0c3cc35e12c7d0cd2ed1317116
Mellomanprost <anoppa@mail.ru>
2022-11-05 13:28:10 +0300

    Добавьте файлы проекта.

---
+++ Unit12.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unit12._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            User user1 = new User("Alex90", "Alex", true);
+            User user2 = new User("Maximus", "Maxim", false);
+            User user3 = new User("Gurman", "Gosha", true);
+            User user4 = new User("Sunshine", "Ann", false);
+            users.Add(user1);
+            users.Add(user2);
+            users.Add(user3);
+            users.Add(user4);
+
+            foreach(var user in users)
+            {
+                Console.WriteLine($"Добро пожаловать {user.Name}!");
+                if (!user.IsPremium)
+                {
+                    Console.WriteLine("Для продолжения работы дождитесь окончания рекламы.");
+                    User.ShowAds();
+                }
+                Console.WriteLine("Вход в систему осуществлен!\n");
+
+            }
+        }
+    }
+}
---
+++ Unit12.1/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Unit12._1._5
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+		public User(string login, string name, bool ispremium)
+        {
+			Login = login;
+			Name = name;
+			IsPremium = ispremium;
+        }
+		public static void ShowAds()
+		{
+			Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+			// Остановка на 1 с
+			Thread.Sleep(1000);
+
+			Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+			// Остановка на 2 с
+			Thread.Sleep(2000);
+
+			Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+			// Остановка на 3 с
+			Thread.Sleep(3000);
+		}
+	}
+
+
+}
